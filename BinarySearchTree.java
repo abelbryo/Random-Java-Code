2c590e527aa0b2da3f3e94e1874c37b26c1925f0
Abel Terefe <abelbryo@gmail.com>
2012-10-18 15:47:44 +0300

    Added TextIO

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,5 +1,7 @@
 public class BinarySearchTree{
 
+    private Node root;
+
     private static class Node{
         Node right;
         Node left;
@@ -8,7 +10,7 @@ public class BinarySearchTree{
         String value;
 
 
-         Node(Node right, Node left, Node parent, int key, String value){
+         Node(Node left, Node right, Node parent, int key, String value){
              this.right = right;
              this.left = left; 
              this.parent = parent;
@@ -22,12 +24,57 @@ public class BinarySearchTree{
         }
     }
 
+    public void insert(int key){
+        Node z = new Node(null, null, null, key, null);
+        Node y = null;
+        Node x = root; 
+        while( x != null){
+            y = x; 
+            if( z.key < x.key)
+                x = x.left;
+            else
+                x = x.right;
+        }
+        z.parent = y; 
+        if (y == null)
+            root = z; 
+        else if( z.key < y.key)
+            y.left = z; 
+        else
+            y.right = z;
+    }
+
+    public int min(){
+        Node min = root;
+        while(min != null)
+            min = min.left;
+        return min.parent.key;
+    }
+
+    public int max(){
+        Node max = root; 
+        while(max != null)
+            max = max.right; 
+        return max.parent.key;
+    }
+
 
     public static void main(String [] args) {
         Node node1 = new Node (null, null, null, 1, "Bar");
         Node node2 = new Node(null, null, null, 1, "Foo");
+        System.out.println("Testing equality : " + node1.equals(node2));
+        BinarySearchTree bst = new BinarySearchTree();
+        
+        int input;
+        do{
+            input = TextIO.getInt();
+            bst.insert(input);
+            
+        }while(input > 0);
 
-        System.out.println( node1.equals(node2));
+        System.out.println(bst.min());
+        System.out.println(bst.max());
+
     }
 
 }
